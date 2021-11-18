a70022ed85f66d7e5138db8ff9ae3b412129a753
iheathers <shresthapritam7@gmail.com>
2021-11-19 00:28:41 +0545

    Migrate filesystem to MySQL database

--- models/product.js
+++ models/product.js
@@ -1,92 +1,40 @@
-const fs = require('fs');
-const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
-const rootDir = require('../utils/path');
-
-const filePath = path.join(rootDir, 'data', 'products.json');
+const db = require('../utils/database');
 
 class Product {
   constructor(id, title, price, imageURL, description) {
-    this.id = id;
+    this.id = uuidv4();
     this.title = title;
     this.price = price;
     this.imageURL = imageURL;
     this.description = description;
   }
 
-  save() {
-    fs.readFile(filePath, (err, data) => {
-      let products = [];
-
-      if (!err && data.length > 0) {
-        products = JSON.parse(data);
-      }
-
-      if (this.id) {
-        const productIndex = products.findIndex((item) => item.id === this.id);
-        products[productIndex] = this;
-      } else {
-        this.id = uuidv4();
-        products.push(this);
-      }
-
-      fs.writeFileSync(filePath, JSON.stringify(products));
-    });
+  async save() {
+    await db.execute(
+      'INSERT INTO products (id, title, price, imageURL, description) VALUES (?, ?, ?, ?, ?)',
+      [this.id, this.title, this.price, this.imageURL, this.description]
+    );
   }
 
-  static deleteProductWithId(id) {
-    fs.readFile(filePath, (err, data) => {
-      let products = [];
-
-      if (!err && data.length > 0) {
-        products = JSON.parse(data);
-      }
-
-      const productIndex = products.findIndex((item) => item.id === id);
-      products.splice(productIndex, 1);
-
-      fs.writeFileSync(filePath, JSON.stringify(products));
-    });
+  static async deleteProductWithId(id) {
+    await db.execute('DELETE FROM products WHERE id = ?', [id]);
   }
 
   static async fetchProductWithId(id) {
-    return new Promise((resolve, reject) => {
-      try {
-        fs.readFile(filePath, (err, data) => {
-          if (data) {
-            const products = JSON.parse(data);
-            const product = products.find((item) => item.id === id);
+    const [result, fields] = await db.execute(
+      'SELECT * FROM products WHERE id = ?',
+      [id]
+    );
 
-            resolve(product);
-          }
-          resolve({
-            id: null,
-            title: 'null',
-            price: 0,
-            description: null,
-            imageURL: null,
-          });
-        });
-      } catch {
-        reject(err);
-      }
-    });
+    if (result.length > 0) {
+      return result[0];
+    }
   }
 
   static async fetchAll() {
-    return new Promise((resolve, reject) => {
-      try {
-        fs.readFile(filePath, (err, data) => {
-          if (data.length > 0) {
-            resolve(JSON.parse(data));
-          }
-          resolve([]);
-        });
-      } catch {
-        reject(err);
-      }
-    });
+    const [result, fields] = await db.execute('SELECT * FROM products');
+    return result;
   }
 }
 
